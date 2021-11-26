a2416a7d3f43ec5fd20adb6795a8bf1f2e7db938
Marciano Priebe <marciano.priebe@gmail.com>
2021-11-25 23:17:51 -0300

    Commit inicial

---
+++ create_host.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+from zabbix_api import ZabbixAPI
+import csv
+
+URL = 'http://{ip}}'
+USERNAME = 'user'
+PASSWORD = 'password'
+
+try:
+    zapi = ZabbixAPI(URL, timeout=15)
+    zapi.login(USERNAME, PASSWORD)
+    print(f'Conectado na API do Zabbix, versao atual {zapi.api_version()}')
+except Exception as err:
+    print(f'Falha ao conectar na API do Zabbix, erro: {err}')
+
+ 
+f = csv.reader(open('hosts.csv'), delimiter=';') #lendo-a-lista de host e separando pelo delimitador ';'
+
+for [hostname,dns] in f:
+
+   zapi.host.create({"host": hostname, 
+      "interfaces": [ {"type": "1",
+      "main": "1",
+      "useip": "0",
+      "ip":"",
+      "dns": dns,
+      "port": "10050"}], 
+      "groups": [{ "groupid": "28"}], #id do host grupo
+      "templates": [{ "templateid":"10436"}], #id do template
+      "tags": [{
+            "tag": "SERVICOS",
+            "value": "www"}],
+
+   })
+   print(f'Host: [{hostname}] cadastrado com sucesso no Zabbix.')
